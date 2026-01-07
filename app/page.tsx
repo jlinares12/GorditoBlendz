684817ae4a3d5536609af043ebfa31551153e833
Juan Linares <juanlinares821@csu.fullerton.edu>
2026-01-06 16:58:08 -0800

    Added the hero to the main page and styled the page to include gaps between components

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar"
 
 export default function Home() {
   return (
-      <main>
+      <main className="flex flex-col p-4 gap-12">
         <Navbar/>
         <Hero/>
       </main>
