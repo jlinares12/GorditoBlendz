e07d9b2c5b73c75e5df2b1da48fe8eb1e3faa1f9
Juan Linares <juanlinares821@csu.fullerton.edu>
2026-01-09 19:02:28 -0800

    Added the specialites section to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar"
+import Specialties from "./components/Specialties";
 
 export default function Home() {
   return (
       <main className="flex flex-col p-4 gap-12">
         <Navbar/>
         <Hero/>
+        <Specialties/>
       </main>
   );
 }
