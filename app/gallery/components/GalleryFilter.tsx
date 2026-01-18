1b1531bc8ef8c4b15d1cd7ccd037161a2c2205c9
Juan Linares <juanlinares821@csu.fullerton.edu>
2026-01-18 11:41:28 -0800

    Made the buttons in the filter toggle between active and inactive

--- app/gallery/components/GalleryFilter.tsx
+++ app/gallery/components/GalleryFilter.tsx
@@ -1,22 +1,64 @@
+'use client';
+import { useState } from "react"
+
 export default function GalleryFilter() {
+
+    const [allActive, setAllActive] = useState(true);
+    const [fadesActive, setFadesActive] = useState(false);
+    const [designsActive, setDesignsActive] = useState(false);
+    const [tapersActive, setTapersActive] = useState(false);
+
+    const activeButton = "flex h-10 shrink-0 items-center justify-center gap-x-2 rounded-full bg-primary px-5 transition-all shadow-md shadow-primary/20"
+    const inactiveButton = "flex h-10 shrink-0 items-center justify-center gap-x-2 rounded-full bg-background border border-white/5 px-5 hover:bg-surface transition-all group"
+    const activeText = "text-white text-sm font-bold leading-normal"
+    const inactiveText = "text-foreground text-sm font-medium leading-normal"
+    const activeSymbol = "material-symbols-outlined text-white"
+    const inactiveSymbol = "material-symbols-outlined text-gold transition-colors"
+
     return (
         <div className="sticky top-[65px] z-40 py-4 md:mx-0 md:px-0">
             <div className="flex gap-3 overflow-x-auto pb-2 md:flex-wrap">
-                <button className="flex h-10 shrink-0 items-center justify-center gap-x-2 rounded-full bg-primary px-5 transition-all hover:bg-primary shadow-md shadow-primary/20">
-                    <span className="material-symbols-outlined text-white">grid_view</span>
-                    <p className="text-white text-sm font-bold leading-normal">All Work</p>
+                <button onClick={ () => {
+                    setAllActive(true)
+                    setFadesActive(false)
+                    setDesignsActive(false)
+                    setTapersActive(false)}
+                } className={allActive ? activeButton : inactiveButton} >
+                    <span className={allActive ? activeSymbol : inactiveSymbol}>grid_view</span>
+                    <p className={allActive ? activeText : inactiveText}>All Work</p>
                 </button>
-                <button className="flex h-10 shrink-0 items-center justify-center gap-x-2 rounded-full bg-background border border-white/5 px-5 hover:bg-surface transition-all group">
-                    <span className="material-symbols-outlined text-gold transition-colors">content_cut</span>
-                    <p className="text-foreground text-sm font-medium leading-normal">Fades</p>
+
+                <button
+                    onClick={ () => {
+                    setAllActive(false)
+                    setFadesActive(true)
+                    setDesignsActive(false)
+                    setTapersActive(false)}
+                } className={fadesActive ? activeButton : inactiveButton}>
+                    <span className={fadesActive ? activeSymbol : inactiveSymbol}>content_cut</span>
+                    <p className={fadesActive ? activeText : inactiveText}>Fades</p>
                 </button>
-                <button className="flex h-10 shrink-0 items-center justify-center gap-x-2 rounded-full bg-background border border-white/5 px-5 hover:bg-surface transition-all group">
-                    <span className="material-symbols-outlined text-gold transition-colors">brush</span>
-                    <p className="text-foreground text-sm font-medium leading-normal">Designs</p>
+
+                <button
+                    onClick={ () => {
+                    setAllActive(false)
+                    setFadesActive(false)
+                    setDesignsActive(true)
+                    setTapersActive(false)}
+                } className={designsActive ? activeButton : inactiveButton}>
+                    <span className={designsActive ? activeSymbol : inactiveSymbol}>brush</span>
+                    <p className={designsActive ? activeText : inactiveText}>Designs</p>
                 </button>
-                <button className="flex h-10 shrink-0 items-center justify-center gap-x-2 rounded-full bg-background border border-white/5 px-5 hover:bg-surface transition-all group">
-                    <span className="material-symbols-outlined text-gold transition-colors">straighten</span>
-                    <p className="text-foreground text-sm font-medium leading-normal">Tapers</p>
+
+                <button
+                    onClick={ () => {
+                    setAllActive(false)
+                    setFadesActive(false)
+                    setDesignsActive(false)
+                    setTapersActive(true)}
+                } className={tapersActive ? activeButton : inactiveButton}>
+                    <span className={tapersActive ? activeSymbol : inactiveSymbol}>straighten</span>
+                    <p className={tapersActive ? activeText : inactiveText}>Tapers</p>
                 </button>
             </div>
         </div>
