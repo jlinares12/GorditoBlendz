f092310bd983de4dc0a643d5efa5e082ab54d7a3
Juan Linares <juanlinares821@yahoo.com>
2026-02-20 02:14:43 -0800

    Using cloudinary pictures and deleted public/preview directory since it is no longer needed

--- app/components/PreviewGallery.tsx
+++ app/components/PreviewGallery.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image'
 import CldImage from './cloudinary/CldImage'
 
 export default async function PreviewGallery() {
@@ -6,8 +5,8 @@ export default async function PreviewGallery() {
         <div className="grid grid-cols-2 gap-4 md:grid-cols-4 md:gap-4 auto-rows-[200px]">
             <div className="col-span-2 row-span-2 group relative overflow-hidden rounded-xl">
                 <div className="absolute inset-0 bg-cover bg-center transition duration-500 group-hover:scale-105" data-alt="Close up of a design from the back profile" >
-                    <Image
-                        src="/preview/design.jpg"
+                    <CldImage
+                        src="ubqujehxiasduattasfb"
                         fill
                         alt="Close up of a design from the back profile"
                         className="object-cover object-center"
@@ -21,7 +20,7 @@ export default async function PreviewGallery() {
             <div className="group relative overflow-hidden rounded-xl">
                 <div className="absolute inset-0 bg-cover bg-center transition duration-500 group-hover:scale-105" data-alt="Side profile of a taper fade/mullet">
                     <CldImage
-                            src="https://res.cloudinary.com/dwwvgtx5m/image/upload/v1769117355/geogpyp4staqolweohja.jpg"
+                            src="fpbbzstgonax8itx0obq"
                             fill
                             alt="Side profile of a taper fade/mullet"
                             className="object-cover object-center"
@@ -34,8 +33,8 @@ export default async function PreviewGallery() {
             </div>
             <div className="group relative overflow-hidden rounded-xl">
                 <div className="absolute inset-0 bg-cover bg-center transition duration-500 group-hover:scale-105" data-alt="Man with a buzz cut and a high fade">
-                    <Image
-                            src="/preview/design_side2.jpg"
+                    <CldImage
+                            src="y97i94kjqmtyhg7u6klc"
                             fill
                             alt="Man with a buzz cut and a high fade"
                             className="object-cover object-center"
@@ -48,8 +47,8 @@ export default async function PreviewGallery() {
             </div>
             <div className="group relative overflow-hidden rounded-xl">
                 <div className="absolute inset-0 bg-cover bg-center transition duration-500 group-hover:scale-105" data-alt="Side profile of a taper fade with a design">
-                    <Image
-                            src="/preview/design_side.jpg"
+                    <CldImage
+                            src="uersrcx5nhm8zfmsrywz"
                             fill
                             alt="Side profile of a taper fade with a design"
                             className="object-cover object-center"
@@ -62,8 +61,8 @@ export default async function PreviewGallery() {
             </div>
             <div className="group relative overflow-hidden rounded-xl">
                 <div className="absolute inset-0 bg-cover bg-center transition duration-500 group-hover:scale-105" data-alt="Back and side profile of a mid fade">
-                    <Image
-                            src="/preview/fade.jpg"
+                    <CldImage
+                            src="mp1ixq1jb29epctri3d2"
                             fill
                             alt="Back and side profile of a mid fade"
                             className="object-cover object-center"
--- app/gallery/components/GalleryPage.tsx
+++ app/gallery/components/GalleryPage.tsx
@@ -4,27 +4,27 @@ export default async function GalleryPage() {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             <GalleryItem
-                src = "/preview/design.jpg"
-                alt = "Close up of a clean skin fade haircut from the side"
+                src="ubqujehxiasduattasfb"
+                alt="Close up of a clean skin fade haircut from the side"
             />
             <GalleryItem
-                src = "/preview/design_side2.jpg"
-                alt = "Close up of a clean skin fade haircut from the side"
+                src="y97i94kjqmtyhg7u6klc"
+                alt="Close up of a clean skin fade haircut from the side"
             />
             <GalleryItem
-                src = "/preview/design_side.jpg"
-                alt = "Close up of a clean skin fade haircut from the side"
+                src="uersrcx5nhm8zfmsrywz"
+                alt="Close up of a clean skin fade haircut from the side"
             />
             <GalleryItem
-                src = "/preview/fade.jpg"
-                alt = "Close up of a clean skin fade haircut from the side"
+                src="mp1ixq1jb29epctri3d2"
+                alt="Close up of a clean skin fade haircut from the side"
             />
             <GalleryItem
-                src = "https://res.cloudinary.com/dwwvgtx5m/image/upload/v1769117845/qjquxym3eovvz3zmf7cg.jpg"
-                alt = "Close up of a clean skin fade haircut from the side"
+                src="y97i94kjqmtyhg7u6klc"
+                alt="Close up of a clean skin fade haircut from the side"
             />
             <GalleryItem
-                src = "/design1.jpg"
+                src = "ubqujehxiasduattasfb"
                 alt = "Close up of a clean skin fade haircut from the side"
             />
         </div>
